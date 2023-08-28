4b495853a8404a40e68b83c89af1036a8065c203
LBolechow <110845720+LBolechow@users.noreply.github.com>
2023-08-28 19:14:13 +0200

    Update

--- src/main/java/pl/lukbol/plugin2/RozwijanyPasek.java
+++ src/main/java/pl/lukbol/plugin2/RozwijanyPasek.java
@@ -1,68 +1,76 @@
 package pl.lukbol.plugin2;
 
+import com.intellij.openapi.fileEditor.FileDocumentManagerAdapter;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.*;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.treeStructure.Tree;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 import java.awt.*;
-import java.io.File;
 
 public class RozwijanyPasek implements ToolWindowFactory {
 
-    private DefaultListModel<String> listModel;
-    private JBList<String> fileList;
-    private VirtualFileListener fileListener;
+    private DefaultTreeModel treeModel;
+    private Tree fileListTree;
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        listModel = new DefaultListModel<>();
-        updateFileList(project);
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode(project.getBasePath());
+        treeModel = new DefaultTreeModel(root);
 
-        fileList = new JBList<>(listModel);
+        updateFileList(project, root, project.getBaseDir());
 
-        fileList.addListSelectionListener(new ListSelectionListener() {
+        fileListTree = new Tree(treeModel);
+
+        VirtualFileListener fileListener = new VirtualFileListener() {
             @Override
-            public void valueChanged(ListSelectionEvent e) {
-                if (!e.getValueIsAdjusting()) {
-                    String selectedFileName = fileList.getSelectedValue();
-                    if (selectedFileName != null) {
-                        VirtualFile selectedFile = LocalFileSystem.getInstance().findFileByPath(project.getBasePath() + "/" + selectedFileName);
-                        if (selectedFile != null) {
-                            OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, selectedFile);
-                            openFileDescriptor.navigate(true);
+            public void fileCreated(@NotNull VirtualFileEvent event) {
+                updateFileList(project, root, project.getBaseDir());
+            }
+
+            @Override
+            public void fileDeleted(@NotNull VirtualFileEvent event) {
+                updateFileList(project, root, project.getBaseDir());
+            }
+        };
+
+        VirtualFileManager.getInstance().addVirtualFileListener(fileListener);
+
+        fileListTree.addTreeSelectionListener(new TreeSelectionListener() {
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                TreePath selectedPath = e.getPath();
+                if (selectedPath != null) {
+                    DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) selectedPath.getLastPathComponent();
+                    if (selectedNode != null) {
+                        Object userObject = selectedNode.getUserObject();
+                        if (userObject instanceof VirtualFile) {
+                            VirtualFile selectedFile = (VirtualFile) userObject;
+                            if (!selectedFile.isDirectory()) {
+                                OpenFileDescriptor openFileDescriptor = new OpenFileDescriptor(project, selectedFile);
+                                openFileDescriptor.navigate(true);
+                            }
                         }
                     }
                 }
             }
         });
 
-        fileListener = new VirtualFileAdapter() {
-            @Override
-            public void fileCreated(@NotNull VirtualFileEvent event) {
-                updateFileList(project);
-            }
-
-            @Override
-            public void fileDeleted(@NotNull VirtualFileEvent event) {
-                updateFileList(project);
-            }
-        };
-
-        VirtualFileManager.getInstance().addVirtualFileListener(fileListener);
-
-        JBScrollPane scrollPane = new JBScrollPane(fileList);
+        JScrollPane scrollPane = new JBScrollPane(fileListTree);
 
         JBPanel panel = new JBPanel(new BorderLayout());
         panel.add(scrollPane, BorderLayout.CENTER);
@@ -70,26 +78,23 @@ public class RozwijanyPasek implements ToolWindowFactory {
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
         Content content = contentFactory.createContent(panel, "", false);
         toolWindow.getContentManager().addContent(content);
-
     }
 
-    private void updateFileList(Project project) {
-        listModel.clear();
-        String projectPath = project.getBasePath();
-        if (projectPath != null) {
-            File projectDir = new File(projectPath);
-            if (projectDir.exists() && projectDir.isDirectory()) {
-                File[] files = projectDir.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.isFile()) {
-                            listModel.addElement(file.getName());
-                        }
-                    }
-                }
+    private void updateFileList(Project project, DefaultMutableTreeNode parent, VirtualFile directory) {
+        parent.removeAllChildren();
+
+        for (VirtualFile file : directory.getChildren()) {
+            DefaultMutableTreeNode node;
+            if (file.isDirectory()) {
+                node = new DefaultMutableTreeNode(file);
+                parent.add(node);
+                updateFileList(project, node, file);
+            } else {
+                node = new DefaultMutableTreeNode(file);
+                parent.add(node);
             }
         }
+
+        treeModel.reload(parent);
     }
-
-
 }
